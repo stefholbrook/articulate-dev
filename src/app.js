9ed6d43c1c402dbf81d76b346421f1c79e251772
Stefanie Holbrook <stefholbrook@gmail.com>
2018-05-26 15:12:47 -0600

    Removed unnecessary code, rendering box with border and shadow

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
+import styled from 'styled-components'
 
-const App = () => <h1>no time for wondering why</h1>
+const Wrapper = styled.div`
+  margin: 50px;
+`
 
+const Card = styled.div`
+  display: flex;
+  flex-flow: column wrap;
+  padding: 60px 40px 50px;
+  border: 1px solid #ddd;
+  box-sizing: border-box;
+  box-shadow: 0 0.4rem 1.2rem 0.2rem rgba(0,0,0,.05);
+`
+
+const App = () => {
+  return (
+    <Wrapper>
+      <Card>
+        I don't think I need that
+      </Card>
+    </Wrapper>
+  )
+}
 export default hot(module)(App)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import 'babel-polyfill'
+// import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
+import { injectGlobal } from 'styled-components'
 
 import App from './app'
+
+injectGlobal`
+  margin: 0 auto;
+  width: 100px;
+`
 
 render(
   <App />,
